0a25e7aba49012e63dd04e0697cb19a90dd17817
LuisCorrea07 <97034439+LuisCorrea07@users.noreply.github.com>
2023-05-11 19:50:50 -0300

    modified:   index.html 	modified:   js/mi-archivo.js

--- js/mi-archivo.js
+++ js/mi-archivo.js
@@ -1,5 +1,10 @@
+let nombre = "";
+alert("Ingresa tu nombre por favor: ");
+nombre = prompt(nombre);
+alert("Bienvenido! " + nombre + " disfruta de la app");
+
 const pantalla = document.querySelector(".pantalla");
-const botones =document.querySelector(".btn");
+const botones = document.querySelectorAll(".btn");
 
 botones.forEach(boton =>{
     boton.addEventListener("click", ()=>{
@@ -10,13 +15,23 @@ botones.forEach(boton =>{
             return;
         }
         if(boton.id === "borrar"){
-           if(pantalla.textContent.length===1){
+           if(pantalla.textContent.length===1 || pantalla.textContent === "Error!"){
             pantalla.textContent ="0"
            }else{
             pantalla.textContent = pantalla.textContent.slice(0, -1);
-               return;
            }
+            return;
         }
+
+        if(boton.id === "igual"){
+            try {
+                pantalla.textContent = eval(pantalla.textContent);    
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+
         if(pantalla.textContent === "0"){
             pantalla.textContent = botonApretado;    
         }else{
